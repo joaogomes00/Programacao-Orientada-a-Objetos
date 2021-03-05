552748227a600febd2d105d060845993a5599b6b
João Pedro <79864997+jpzin10@users.noreply.github.com>
2021-03-04 21:47:47 -0300

    Add files via upload
    Faça um programa em Java (Tabuada.java) para imprimir a tabuada de 10.

---
+++ Tabuada.java
@@ -0,0 +1,14 @@
+/*
+*Nome: João Pedro Gomes de Lemos
+*Matrícula: 0050016060
+*/ 
+
+class Tabuada {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i<=10; i++){
+            System.out.printf("\n%d * 10 = %d",i,i*10);
+        }
+    }
+}
